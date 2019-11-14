d46cbc953dfddfa9e96658de909c4dd4b18b1ab1
Cesar Alzate <nearbevi@gmail.com>
2019-11-13 21:30:58 -0500

    Update displayTopAndBottom.h

--- display/displayTopAndBottom.h
+++ display/displayTopAndBottom.h
@@ -32,7 +32,6 @@ int p2b[5][5] = {
   {1,0,1,0,1}
   };
 
-// make two fucntions into one. player "1" is a variable
 void displayBoards(int tb[][7], int bb[][7], int row, int col, int PlayerNumber){
 	cout << "Player " << PlayerNumber << " Top Board: " << endl;
 	for (int i =0; i < row; i++){
