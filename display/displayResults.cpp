c9f900e98799771f6e85790dd6f2c251339da296
whvod <nearbevi@gmail.com>
2019-11-18 21:04:30 -0500

    display Results commit

--- display/displayResults.cpp
+++ display/displayResults.cpp
@@ -2,8 +2,14 @@
 
 void displayResults(){
 	
-	cout << "Player " << player1 << " Wins" << endl;
-	cout << "Player " << player2 << ", better luck next time" << endl;
+	cout << "Player " << currentPlayer << " Wins" << endl;
+	if (currenPlyayer == 0){ // player 0 is player 1
+	cout << "Player 2" <<  << ", better luck next time" << endl;
+	}
+	if(currentPlayer == 1){
+	cout << "Player 1" <<  << ", better luck next time" << endl;
+	}
+
 }
 
 int main(){
