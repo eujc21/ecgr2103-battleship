a8953b81bfe34c90ffa928f094d6171d7b208aca
whvod <nearbevi@gmail.com>
2019-11-18 20:50:13 -0500

    display Results commit

---
+++ display/displayResults.cpp
@@ -0,0 +1,12 @@
+
+
+void displayResults(){
+	
+	cout << "Player " << player1 << " Wins" << endl;
+	cout << "Player " << player2 << ", better luck next time" << endl;
+}
+
+int main(){
+
+	return 0;
+}
