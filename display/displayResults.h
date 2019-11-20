fdb8ac0aa60cae19ebe15efef6f24a4858bada64
whvod <nearbevi@gmail.com>
2019-11-19 22:27:32 -0500

    formatting for display results

--- display/displayResults.h
+++ display/displayResults.h
@@ -3,7 +3,10 @@
 using namespace std;
 
 void displayResults(int currentPlayer){
-
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
 	cout << "Player " << (currentPlayer + 1) << " Wins" << endl;
 	if (currentPlayer == 0){ // player 0 is player 1
 		cout << "Player 2" << ", better luck next time" << endl;
@@ -11,6 +14,10 @@ void displayResults(int currentPlayer){
 	if(currentPlayer == 1){
 		cout << "Player 1" << ", better luck next time" << endl;
 	}
+	cout << "Play again!" << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
 
 }
 
